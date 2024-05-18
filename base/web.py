6a5536fc458112b1fe041ca06cdbae5e6a8a13d5
WangYun <294961525@qq.com>
2024-05-18 20:53:15 +0800

    240518: update web keys

--- base/web.py
+++ base/web.py
@@ -1,10 +1,151 @@
-from time import sleep
-from typing import Any
+import json
+import time
+from typing import List
+from config.log import get_logger
+from pathlib import Path
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.common.exceptions import WebDriverException
 
 
 class WebKeys:
-    def __init__(self, driver):
+    def __init__(self, driver: WebDriver):
         self.driver = driver
+        self.logger = get_logger()
+
+    def click(
+        self,
+        locator: str | tuple | WebElement,
+        index: int | None = 0,
+        use_action_chain: bool = False,
+        root: WebDriver | WebElement | None = None,
+        wait_to: float | int = 0.1,
+    ) -> bool:
+        elements = self.get_elements(locator, index=index, root=root)
+        if not elements:
+            msg = f"no element with {locator} is found"
+            self.logger.error(msg)
+            raise ValueError(msg)
+
+        def click_one(_elem):
+            if use_action_chain:
+                self.logger.debug("click use action chain")
+                ActionChains(self.driver).move_to_element(_elem).perform()
+            time.sleep(wait_to)
+            _elem.click()
+            return True
+
+        if isinstance(elements, list):
+            for elem in elements:
+                if click_one(elem):
+                    return True
+        else:
+            return click_one(elements)
+
+    def dump_cookies(self, path: str = "config/auto-qa.cookies"):
+        # Note: If "auto-qa.cookies" already exists, the original information will be overwritten when this method is called again.
+        cookies = self.driver.get_cookies()
+        self.logger.debug(f"dumped {len(cookies)} cookies to {path}")
+        Path(path).write_text(json.dumps(cookies, indent=4))
+
+    def get_elements(
+        self, locator: str | tuple | WebElement, index: int | None = 0, root: WebDriver | WebElement | None = None
+    ) -> WebElement | List[WebElement] | None:
+        """
+        first of all get_elements will always find multiple elems,
+        if index is None, will return all found elems
+        else just return the element with supplied index.
+
+        there 3 types of return value:
+            1. nothing found, just return elems which is []
+            2. index is None = multiple elems, just return elems
+            3. index is int = not multiple, just return elems[index]
+        """
+        self.logger.debug(f"find elements by {locator}")
+        if not isinstance(locator, tuple) and not isinstance(locator, str):
+            return locator
+
+        if isinstance(locator, str):
+            locator = (By.CSS_SELECTOR, locator)
+
+        root = root or self.driver
+        try:
+            elements = root.find_elements(*locator)
+
+            if index is None:
+                return elements
+
+            if elements and isinstance(index, int):
+                return elements[index]
+        except WebDriverException as e:
+            self.logger.error(f"get {locator} failed with {e}")
+            raise
+
+    def load_cookies(self, path: str = "config/auto-qa.cookies"):
+        cookie_file = Path(path)
+        if not cookie_file.exists():
+            self.logger.debug(f"cookie not found locally, please check the path: {cookie_file}")
+            return
+        data = cookie_file.read_text()
+        cookies = json.loads(data)
+        self.logger.debug(f"loaded {len(cookies)} cookies to driver")
+        for ck in cookies:
+            self.driver.add_cookie(ck)
+
+    def scroll_and_click(self, locator: str | tuple | WebElement, index=0, root=None) -> bool:
+        element = self.get_elements(locator, index=index, root=root)
+        if not element:
+            msg = f"no with {locator} is found"
+            self.logger.error(msg)
+            raise ValueError(msg)
+
+        self.scroll_to_element(element)
+        return self.click(element)
+
+    def scroll_to_element(self, element: WebElement, is_window=True, steps: float | int = 8):
+        try:
+            element_location = element.location['y']
+        except Exception:
+            return element.location_once_scrolled_into_view
+
+        element_location = element_location - 130
+        if element_location < 0:
+            element_location = 0
+        if is_window:
+            scroll_script = "window.scrollTo(0, %s);" % element_location
+        else:
+            scroll_script = "(arguments[0]).scrollBy(0, {})".format(steps)
+
+        try:
+            self.driver.execute_script(scroll_script, element)
+        except WebDriverException:
+            pass
+
+    def update_text(
+        self,
+        locator: str | tuple | WebElement,
+        value: str,
+        index: int | None = 0,
+        root: WebDriver | WebElement | None = None,
+        as_human: bool = False,
+    ):
+        elem = self.get_elements(locator, index=index, root=root)
+        if not elem:
+            msg = f"no input element with {locator} is found"
+            self.logger.error(msg)
+            raise ValueError(msg)
+
+        if not isinstance(elem, WebElement):
+            msg = f"{locator=}'s should only find one WebElement"
+            self.logger.error(msg)
+            raise TypeError(f"{locator=}'s should only find one WebElement")
+
+        elem.send_keys(value)
+        return True
+
+    #########
 
     def accept_alert(self, text: str | None = None):
         _alert = self.driver.switch_to.alert
@@ -15,9 +156,6 @@ class WebKeys:
     def assert_text_equal(self, locator: tuple, expect: str):
         actual = self.locator(locator).text
         assert actual == expect, f"actual: {actual} != expect: {expect}"
-
-    def click(self, locator):
-        self.locator(locator).click()
 
     def close(self):
         self.driver.quit()
@@ -37,11 +175,5 @@ class WebKeys:
     def input(self, locator: tuple, txt: str):
         self.locator(locator).send_keys(txt)
 
-    def locator(self, locator: tuple):
-        return self.driver.find_element(*locator)
-
     def open(self, url: str):
         self.driver.get(url)
-
-    def wait(self, times: Any):
-        sleep(int(times))
