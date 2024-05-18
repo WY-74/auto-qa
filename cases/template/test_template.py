05796cb5a78d0f5bcf8f0297d0c5f0cb20eacb29
WangYun <294961525@qq.com>
2024-05-18 15:58:55 +0800

    240518: update setUpClass

--- cases/template/test_template.py
+++ cases/template/test_template.py
@@ -4,5 +4,9 @@ from cases.base_case import BaseCase
 
 @ddt
 class TestBaidu(BaseCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
     @file_data("../../data/template/template.yaml")
     def test_01_search(self, **kwargs): ...
