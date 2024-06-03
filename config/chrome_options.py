dc60c3f9acdc4f2945a4e91bca5409d1c101bd24
WangYun <294961525@qq.com>
2024-06-03 16:48:50 +0800

    240603: refactor chrome_option

--- config/chrome_options.py
+++ config/chrome_options.py
@@ -1,48 +1,47 @@
+import configparser
 from selenium import webdriver
 from config.log import logger
 
 
-def chrome_options():
+def chrome_options(headless: bool = False, download_to: str = ""):
     options = webdriver.ChromeOptions()
 
     options.page_load_strategy = "normal"
 
     options.add_experimental_option("excludeSwitches", ["enable-automation", "enable-logging"])
-    options.add_experimental_option(
-        "prefs", {"credentials_enable_service": False, "profile.password_manager_enable": False}
-    )
+    prefs = {
+        "profile.password_manager_enabled": False,
+        "credentials_enable_service": False,
+        "download.default_directory": download_to,
+    }
+    options.add_experimental_option("prefs", prefs)
 
     options.add_argument("start-maximized")
     options.add_argument("--log_level=3")
     options.add_argument("--disable-gup")
     options.add_argument("--ignore-certificate-errors")
+    if bool(headless):
+        options.add_argument("--headless")
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
 
     logger.debug(f"{options.to_capabilities()}")
-
     return options
 
 
-def get_driver(driver_type: str | None = None):
+def get_driver():
     """
-    bowser_type:
-        Chrome: Chrome, chrome, Googel Chrome, google chrome
+    Supported Browsers:
+        - Chrome
     """
+    _config = configparser.ConfigParser()
+    _config.read('.\pytest.ini', encoding="utf-8")
+    _config = dict(_config.items("driver"))
+    headless = False if _config["headless"] == "False" else True
+    download_to = _config["download_to"]
 
-    browser_map = {"Chrome": ["Chrome", "chrome", "Googel Chrome", "google chrome"]}
-    driver = None
-
-    for key, value in browser_map.items():
-        if driver_type in value:
-            driver_type = key
-            driver = getattr(webdriver, driver_type)()
-            break
-
-    if driver is None:
-        driver_type = "Chrome"
-        driver = webdriver.Chrome(options=chrome_options())
-
+    driver = webdriver.Chrome(options=chrome_options(headless, download_to))
     driver.implicitly_wait(5)
 
     logger.info("init driver finished")
-
     return driver
